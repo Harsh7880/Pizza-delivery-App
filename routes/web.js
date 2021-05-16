e790eab2a80cf002beb54b50212a546e30f7b305
Harsh kumar <vijayk8478@gmail.com>
2021-05-16 15:56:35 +0530

    changes done

---
+++ routes/web.js
@@ -0,0 +1,19 @@
+const homeController = require('../app/http//controllers/homeController')
+const authController = require('../app/http/controllers/authController')
+const cartController = require('../app/http/controllers/customers/cartConroller')
+
+// routes
+function initRoutes(app){
+    
+    app.get('/', homeController().index)
+    app.get('/login', authController().login)
+    app.get('/register',authController().register)
+    app.get('/cart', cartController().index)
+}
+
+
+
+
+
+
+module.exports = initRoutes
--- server.js
+++ server.js
@@ -3,7 +3,25 @@ const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
+const session = require('express-sessions');
+
+// DataBase Connection
+const url = 'mongodb://localhost/pizza-app';
+
+mongoose.connect(url,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology:true
+});
+   const connection = mongoose.connection;
+   connection.once('open',()=>{
+       console.log('Database Sucessfully Connected.....');
+   }).catch(err =>{
+       console.log('connection failed...');
+   });
 
 // Assets
 app.use(express.static('public'))
@@ -13,27 +31,7 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-
-// routes
-
-app.get('/' ,(req,res) =>{
-    res.render('home')
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
-
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
-
-
-
+require('./routes/web')(app)
 
 // listning to server on port
 app.listen(3000, () =>{
